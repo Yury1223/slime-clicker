9215c850ca08c848e5a4e5a97f8e234afb354c23
Nemuru <noop>
2021-09-21 08:38:42 +0900

    パラメータコンポーネントを追加

---
+++ components/SlimeParameter.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    <h3>{{ context.type }}({{ context.level }})</h3>
+    <p>{{ context.value }}</p>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+export const SLIME_PARAMETER_TYPE = {
+  ATK: 'こうげき',
+  DEF: 'ぼうぎょ',
+  HP: 'HP',
+  AGL: 'すばやさ'
+} as const
+export type SlimeParameterType = typeof SLIME_PARAMETER_TYPE[keyof typeof SLIME_PARAMETER_TYPE]
+
+export default Vue.extend({
+  props: {
+    context: {
+      type: Object,
+      default: () => {
+        return {
+          type: SLIME_PARAMETER_TYPE.HP,
+          value: 0,
+          level: 1
+        }
+      }
+    }
+  }
+})
+</script>
--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,44 @@
 <template>
-  <Tutorial />
+  <div>
+    <SlimeParameter :context="hpContext" />
+    <SlimeParameter :context="atkContext" />
+    <SlimeParameter :context="defContext" />
+    <SlimeParameter :context="aglContext" />
+  </div>
 </template>
 
 <script lang="ts">
 import Vue from 'vue'
+import SlimeParameter from '../components/SlimeParameter.vue'
+import { SLIME_PARAMETER_TYPE } from '~/components/SlimeParameter.vue'
 
-export default Vue.extend({})
+export default Vue.extend({
+  components: {
+    SlimeParameter
+  },
+  data () {
+    return {
+      hpContext: {
+        type: SLIME_PARAMETER_TYPE.HP,
+        value: 255,
+        level: 1
+      },
+      atkContext: {
+        type: SLIME_PARAMETER_TYPE.ATK,
+        value: 255,
+        level: 1
+      },
+      defContext: {
+        type: SLIME_PARAMETER_TYPE.DEF,
+        value: 255,
+        level: 1
+      },
+      aglContext: {
+        type: SLIME_PARAMETER_TYPE.AGL,
+        value: 255,
+        level: 1
+      }
+    }
+  }
+})
 </script>
